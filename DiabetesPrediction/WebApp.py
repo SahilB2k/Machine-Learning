3c5529055530e6f025972734f7ae3c9d26658e76
SahilB2k <jadhavssahil@gmail.com>
2024-07-25 20:28:19 +0530

    modified

--- DiabetesPrediction/WebApp.py
+++ DiabetesPrediction/WebApp.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model=pickle.load(open(r'C:\\Users\sahil jadhav\\OneDrive\\Desktop\\Deployed ML projects\\DiabetesPrediction\\Trained_model.sav','rb')) 
+loaded_model=pickle.load(open(r"C:\Users\sahil jadhav\OneDrive\Desktop\Deployed ML projects\DiabetesPrediction\Trained_model.sav",'rb')) 
 
 
 def diabetes_pred(input_data):
