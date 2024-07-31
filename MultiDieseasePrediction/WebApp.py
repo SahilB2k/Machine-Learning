96a0554c48ae2951f127ffcf98907c93e4e13853
SahilB2k <jadhavssahil@gmail.com>
2024-07-31 11:32:04 +0530

    updated

--- MultiDieseasePrediction/WebApp.py
+++ MultiDieseasePrediction/WebApp.py
@@ -2,11 +2,11 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('Diabetes_model.sav','rb'))
+diabetes_model = pickle.load(open(r'C:\Users\sahil jadhav\OneDrive\Desktop\Deployed ML projects\MultiDieseasePrediction\Diabetes_model.sav','rb'))
 
-heart_disease_model = pickle.load(open('heartDisease_model.sav','rb'))
+heart_disease_model = pickle.load(open(r'C:\Users\sahil jadhav\OneDrive\Desktop\Deployed ML projects\MultiDieseasePrediction\heartDisease_model.sav','rb'))
 
-Parkinson_model = pickle.load(open('Parkinson_model.sav','rb'))
+Parkinson_model = pickle.load(open(r'C:\Users\sahil jadhav\OneDrive\Desktop\Deployed ML projects\MultiDieseasePrediction\Parkinson_model.sav','rb'))
 
 with st.sidebar:
 
@@ -61,7 +61,7 @@ if (selected == 'Diabetes Prediction'):
             diab_diagnosis = 'The Person is Not Diabetic'        
     st.success(diab_diagnosis)
         
-if (selected ==' Heart Disease Prediction'):
+if(selected =='Heart Disease Prediction'):
 
     # page title 
     st.title('Heart Disease Prediction')
@@ -117,6 +117,88 @@ if (selected ==' Heart Disease Prediction'):
         else:
             heart_diagnosis = 'The Person is Normal'        
     st.success(heart_diagnosis)
+
+# Parkinson Prediction
+if(selected =='Parkinson Prediction'):
+
+    # page title 
+    st.title('Parkinson Prediction')
+
+    col1,col2,col3,col4,col5=st.columns(5)
+
+    with col1:
+        fo=st.text_input('MDVP:Fo(Hz)')
+
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+
+    with col3:
+        flo=st.text_input('MDVP:Flo(Hz)')
+
+    with col4:
+        jitter_percent=st.text_input('MDVP:Jitter(%)')
+
+    with col5:
+        jitter_abs=st.text_input('MDVP:Jitter(Abs)')
+
+    with col1:
+        Rap=st.text_input('MDVP:RAP')
+
+    with col2:
+        Ppq=st.text_input('MDVP:PPQ')
+
+    with col3:
+        ddp=st.text_input('Jitter:DDP')
+
+    with col4:
+        shimmer=st.text_input(' MDVP:Shimmer ')
+    
+    with col5:
+        apq3=st.text_input('Shimmer:APQ3 ')
+
+    with col1:
+        apq5=st.text_input(' Shimmer:APQ5  ')
+
+    with col2:
+        apq=st.text_input(' MDVP:APQ ')
+
+    with col3:
+        dda=st.text_input('Shimmer:DDA')
+
+    with col4:
+        nhr=st.text_input('NHR')
+    
+    with col5:
+        hnr=st.text_input('HNR')
+    
+    with col1:
+        rpde=st.text_input('RPDE')
+
+    with col2:
+        dfa=st.text_input('DFA')
+
+    with col3:
+        spread1=st.text_input('Spread1')
+    
+    with col4:
+        spread2=st.text_input('Spread2')
+
+    with col5:
+        d2=st.text_input('D2')
+        
+    with col1:
+        ppe=st.text_input('PPE')
+
+    parkinson_diagnosis=''
+
+    if st.button('Heart Disease Test Result'):
+        parkinson_prediction=Parkinson_model.predict([[fo,fhi,flo,jitter_percent,jitter_abs,Rap,Ppq,ddp,shimmer,apq3,apq5,apq,dda,nhr,hnr,rpde,dfa,spread1,spread2,d2,ppe]]) 
+
+        if (parkinson_prediction==1):
+            parkinson_diagnosis_diagnosis= 'The  Person is having Heart Disease'
+        else:
+            parkinson_diagnosis = 'The Person is Normal'        
+    st.success(parkinson_diagnosis)
 
 
 
